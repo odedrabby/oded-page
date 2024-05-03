dfb723f3bb6076a362e129fca5df4e92f76b9b72
Arik Hativa <arikhativa@gmail.com>
2024-05-03 16:59:36 +0300

    midbug

--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LetterComponent } from '../letter/letter.component';
-import { BodyComponent } from './body.component';
 
 
 
 @NgModule({
     declarations: [
         LetterComponent,
-        BodyComponent
     ],
     imports: [
         CommonModule,
     ],
     exports: [
-        BodyComponent
+        LetterComponent
     ]
 })
 export class BodyModule { }
