7e4bd517963c83e370d7542f2aef49ecf7e16c60
Yoav Rabby <yoavrbb@gmail.com>
2024-05-08 12:26:37 +0300

    better br

--- src/app/play-button/play-button.component.ts
+++ src/app/play-button/play-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SharedService } from '../shared.service';
 import { playPause } from '../music-player/music-player.component';
 
@@ -10,16 +10,10 @@ import { playPause } from '../music-player/music-player.component';
 export class PlayButtonComponent {
     @Input() isPlaying?: boolean;
     @Input() songName?: string;
-    @Input() size?: number;
     iconSize?: number;
 
-    constructor(private ss: SharedService) {}
+    constructor(private ss: SharedService) { }
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes['size']) {
-            this.iconSize = this.size ? this.size / 2 : 0;
-        }
-    }
 
     click = () => {
         if (this.songName) {
