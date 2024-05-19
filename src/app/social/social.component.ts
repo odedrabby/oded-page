599bf664c756caee66729933a929b5abf8bd26d8
Yoav Rabby <yoavrbb@gmail.com>
2024-05-19 17:02:28 +0300

    email, linktree and lyrics

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Styles } from '@fortawesome/fontawesome-svg-core';
 import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faAt } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-social',
@@ -9,27 +9,13 @@ import { faAt } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './social.component.scss'
 })
 export class SocialComponent {
-  faAt = faAt
+  faEnvelope = faEnvelope
   faFacebookF = faFacebookF
   faInstagram = faInstagram
+  email = 'galoscohen@gmail.com'
 
   style: Styles = {
     height: '40px',
     width: '40px'
-  }
-
-  // TODO
-  async copyEmail() {
-    const email = 'galoscohen@gmail.com';
-    try {
-      await navigator.clipboard.writeText(email);
-      console.log('Email copied to clipboard');
-    } catch (err) {
-      console.error('Failed to copy email: ', err);
-    }
-  }
-
-  async click() {
-    await this.copyEmail()
   }
 }
