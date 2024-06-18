fee94e1e8995bbb464a0b0f5370344c65ed0a550
Yoav Rabby <arikhativa@gmail.com>
2024-06-18 10:23:04 +0300

    added download button

--- src/app/letter/letter.component.ts
+++ src/app/letter/letter.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { introParagraph, introTitle, pathToCover } from '../../assets/strings';
+import { discoName, discoPath, introParagraph, introTitle, pathToCover } from '../../assets/strings';
+import { TextButtonComponentIcon } from '../text-button/text-button.component';
 
 @Component({
     selector: 'app-letter',
@@ -12,4 +13,9 @@ export class LetterComponent {
     readonly path = pathToCover
     readonly width = '300px'
     readonly height = '300px'
+    downloadButton = TextButtonComponentIcon.DOWNLOAD
+    discoPath = discoPath
+    discoName = discoName
+
+
 }
